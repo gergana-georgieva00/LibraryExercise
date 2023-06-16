d7701c439f7dd9e4b4f0ad154a38d7f5de262c76
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-16 17:53:35 +0300

    Implemented AllBooksViewModel

---
+++ Library/Models/AllBooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Models
+{
+    public class AllBooksViewModel
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public decimal Rating { get; set; }
+        public int CategoryId { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
