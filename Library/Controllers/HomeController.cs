d1c42342eaa9e713fa0c1635e0cb1d7e0ea86dc3
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-16 19:45:08 +0300

    edit

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace Library.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         [AllowAnonymous]
         public IActionResult Index()
