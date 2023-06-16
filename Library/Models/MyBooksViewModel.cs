6be42c1ed4f77249a5bded96e6aa3fe83d0178b7
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-16 17:56:20 +0300

    Edits

--- Library/Models/MyBooksViewModel.cs
+++ Library/Models/MyBooksViewModel.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Author { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
-        public decimal Rating { get; set; }
         public string Category { get; set; } = null!;
     }
 }
