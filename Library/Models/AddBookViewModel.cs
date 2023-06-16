ea2ed22bd2dcee9ccded0463d8b2cc635c6fb584
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-16 18:16:47 +0300

    Implemented AddBookViewModel

---
+++ Library/Models/AddBookViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class AddBookViewModel
+    {
+        [Required]
+        [MinLength(10), MaxLength(50)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [MinLength(5), MaxLength(50)]
+        public string Author { get; set; } = null!;
+        [Required]
+        [MinLength(5), MaxLength(5000)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+        [Required]
+        [Range(0.0, 10.0)]
+        public decimal Rating { get; set; }
+        [Required]
+        public string Category { get; set; } = null!;
+    }
+}
