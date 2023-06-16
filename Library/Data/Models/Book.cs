2d606ece8bfb9a9e83da0f819b712b0af70202a8
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-16 17:14:47 +0300

    Additions in Book

--- Library/Data/Models/Book.cs
+++ Library/Data/Models/Book.cs
@@ -1,14 +1,30 @@
-﻿namespace Library.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MinLength(10), MaxLength(50)]
         public string Title { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(50)]
         public string Author { get; set; }
+        [Required]
+        [MinLength(5), MaxLength(5000)]
         public string Description { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
+        [Range(0.0, 10.0)]
         public decimal Rating { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
         public Category Category { get; set; }
         public List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
     }
